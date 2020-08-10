b0cd65d1aa0894a8df1ae82fb2b2264fa0faa356
Nisarg Shah <50866039+niSarg1910@users.noreply.github.com>
2020-08-10 14:53:25 +0530

    Add files via upload

---
+++ training.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 14:45:31 2020
+
+@author: DELL
+"""
+
+import numpy as np
+from perceptron import Perceptron
+
+training_inputs = []
+training_inputs.append(np.array([1,1]))
+training_inputs.append(np.array([0,1]))
+training_inputs.append(np.array([1,0]))
+training_inputs.append(np.array([0,0]))
+
+labels = np.array([1,0,0,0])
+
+perceptron = Perceptron(2)
+perceptron.train(training_inputs,labels)
+
+inputs = np.array([1,1])
+print(perceptron.predict(inputs))
+
+inputs = np.array([0,1])
+print(perceptron.predict(inputs))
